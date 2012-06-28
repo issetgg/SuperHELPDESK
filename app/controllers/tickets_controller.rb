abf411843345ea843514c79799b7cd9b3311bc28
Gustavo GM <gozunam@gmail.com>
2012-06-28 11:45:49 -0500

    Removing comments

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,45 +1,38 @@
 
 class TicketsController < ApplicationController
-  # GET /tickets
-  # GET /tickets.json
+
   def index
     @tickets = Ticket.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html 
       format.json { render json: @tickets }
     end
   end
 
-  # GET /tickets/1
-  # GET /tickets/1.json
   def show
     @ticket = Ticket.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html 
       format.json { render json: @ticket }
     end
   end
 
-  # GET /tickets/new
-  # GET /tickets/new.json
   def new
     @ticket = Ticket.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html 
       format.json { render json: @ticket }
     end
   end
 
-  # GET /tickets/1/edit
+
   def edit
     @ticket = Ticket.find(params[:id])
   end
 
-  # POST /tickets
-  # POST /tickets.json
   def create
     @ticket = Ticket.new(params[:ticket])
 
@@ -54,8 +47,6 @@ class TicketsController < ApplicationController
     end
   end
 
-  # PUT /tickets/1
-  # PUT /tickets/1.json
   def update
     @ticket = Ticket.find(params[:id])
 
@@ -70,8 +61,6 @@ class TicketsController < ApplicationController
     end
   end
 
-  # DELETE /tickets/1
-  # DELETE /tickets/1.json
   def destroy
     @ticket = Ticket.find(params[:id])
     @ticket.destroy
