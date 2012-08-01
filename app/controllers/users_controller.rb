e40aa9362cf6d5dc7ea02d4ccd09fbe0b68a8dca
Gustavo GM <gozunam@gmail.com>
2012-08-01 17:22:40 -0500

    Removing show action from users_controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
     respond_to do |format|
       format.html 
       format.json { render json: @users }
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html 
-      format.json { render json: @user }
-    end
-  end
-
   def new
     @user = User.new
-
     respond_to do |format|
       format.html 
       format.json { render json: @user }
@@ -36,7 +25,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
@@ -50,7 +38,6 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -65,7 +52,6 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
